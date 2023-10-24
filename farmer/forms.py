63bc2723dd42953f955f93704dc51ac390951d24
patel436 <patel436@uwindsor.ca>
2023-10-24 15:04:02 -0400

    [AG-41] : Add, update and delete product facility for farmer

--- farmer/forms.py
+++ farmer/forms.py
@@ -1,8 +1,63 @@
 from django import forms
+from store.models import Category, Product  # Import the Category model
 
-class AddProductForm(forms.Form):
-    product_name = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Enter Product Name',
-        'class': 'form-control',
+class AddProductForm(forms.ModelForm):
+    product_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter Product Name',
+            'class': 'form-control',
+        }),
+        label='Product Name'
+    )
 
-    }), label='Product Name')
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={  # Use Textarea for a larger description field
+            'placeholder': 'Enter Product Description',
+            'class': 'form-control',
+        }),
+        label='Product Description'
+    )
+
+    price = forms.DecimalField(
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter Product Price',
+            'class': 'form-control',
+        }),
+        label='Price'
+    )
+
+    image = forms.ImageField(
+        widget=forms.FileInput(attrs={
+            'class': 'form-control',
+        })
+    )
+
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        empty_label="Select a category",
+        widget=forms.Select(attrs={
+            'class': 'form-control',
+        }),
+        label='Category'
+    )
+
+    quantity = forms.IntegerField(
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter Product Quantity',
+            'class': 'form-control',
+        }),
+        label='Quantity'
+    )
+
+    is_available = forms.BooleanField(
+        initial=True,
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'form-check-input',
+        }),
+        label='Available'
+    )
+
+    class Meta:
+        model = Product
+        fields = ['product_name', 'description', 'price', 'image', 'category', 'quantity', 'is_available']
