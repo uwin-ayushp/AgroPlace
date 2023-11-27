f96b2f7fbedcdbf6c141fb471c0bc6824a420299
Harsh Prajapati <harshsp31598@gmail.com>
2023-11-27 16:03:20 -0500

    chatbox

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('resetPassword/', views.resetPassword, name='reset-password'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('order_detail/<int:order_id>/' ,views.order_detail, name='order_detail'),
-    path('chatbox/<int:id>/' ,views.chatbox, name='chatbox')
+    path('chatbox/<int:id>/' ,views.chatbox, name='chatbox'),
+    path('sendMsg/', views.sendMsg, name='sendMsg'),
 ]
--- farmer/models.py
+++ farmer/models.py
@@ -8,11 +8,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Chatbox(models.Model):
-    sp_id = models.IntegerField()
-    farmer_id =models.IntegerField()
-    text=models.CharField(max_length=1000, unique=True)
-    created_date = models.DateTimeField(auto_now_add=True)
 
 class Recommendation(models.Model):
     recommendation_order = models.IntegerField()
