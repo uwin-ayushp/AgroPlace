ce0d05e88bb27272cfe8440efbc2ae7314f8eabd
patel436 <patel436@uwindsor.ca>
2023-12-03 15:30:17 -0500

    [issue/filterOption]

--- store/forms.py
+++ store/forms.py
@@ -5,3 +5,23 @@ class ReviewForms(forms.ModelForm):
     class Meta:
         model = ReviewRating
         fields = ['subject','review','rating']
+
+class ProductSortForm(forms.Form):
+    SORT_CHOICES = [
+        ('new', 'New'),
+        ('price_low_to_high', 'Price: Low to High'),
+        ('price_high_to_low', 'Price: High to Low'),
+    ]
+
+    sort = forms.ChoiceField(choices=SORT_CHOICES, required=False, widget=forms.Select(attrs={'class': 'form-select form-select-sm', 'id': 'sortOptions'}), label='')
+
+class CatSortForm(forms.Form):
+    CAT_CHOICES = [
+        ('allproducts', 'Allproduct'),
+        ('fruits', 'Fruits'),
+        ('grains', 'Grains'),
+        ('seeds', 'Seeds'),
+        ('vegetable', 'Vegetable'),
+    ]
+
+    csort = forms.ChoiceField(choices=CAT_CHOICES, required=False, widget=forms.Select(attrs={'class': 'form-select form-select-sm', 'id': 'catOptions'}), label='')
--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('recommendations/', views.get_recommendations, name='recommendations'),
     path('<slug:category_slug>/',views.store, name='products_by_category'),
     path('submit_review/<int:product_id>/',views.submit_reviews,name='submit_reviews'),
-
-
+    path('home/', views.sortProducts, name='sort_home'),
+    path('home/', views.catProducts, name='catsort_home')
 ]
